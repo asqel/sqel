65806cc6d7580b8852ad10aacf933065a50dab9e
asqool <91128487+asqool@users.noreply.github.com>
2022-09-18 18:20:21 +0200

    m

--- basic.py
+++ basic.py
@@ -9,24 +9,51 @@ DIGITS="1234567890"
 
 
 class Error:
-    def __init__(self,error_name,details):
+    def __init__(self,pos_start,pos_end,error_name,details):
+        self.pos_start=pos_start
+        self.pos_end=pos_end
         self.error_name=error_name
         self.details=details
         
     def as_string(self):
         result=f'{self.error_name}:{self.details}'
+        result+=f" File {self.pos_start.fn}, line {self.pos_start.ln+1}"
         return result
     
 class IllegalCharError(Error):
-    def __init__(self,details):
-        super().__init__("Illegal Character",details)
+    def __init__(self,pos_start,pos_end,details):
+        super().__init__(pos_start,pos_end,"Illegal Character",details)
+
+#######################################
+#position
+
+class Position:
+    def __init__(self,idx,ln,col,fn,ftxt) -> None:
+        self.idx=idx
+        self.col=col
+        self.ln=ln
+        self.fn=fn
+        self.ftxt=ftxt
+
+    def advance(self,current_char):
+        self.idx+=1
+        self.col+=1
+
+        if current_char=="\n":
+            self.ln+=1
+            self.col=0
+        return self
+
+
+    def copy(self):
+        return Position(self.idx,self.ln,self.col,self.fn,self.ftxt)
 
 
 ########################################
 #TOKENS
 
 
-TT_INT="TT_INT"
+TT_INT="INT"
 TT_FLOAT="FLOAT"
 TT_PLUS="PLUS"
 TT_MINUS="MINUS"
@@ -37,7 +64,7 @@ TT_RPAREN="RPAREN"
 
 
 class Token:
-    def __init__(self,type_,value) -> None:
+    def __init__(self,type_,value=None) -> None:
         self.type=type_
         self.value=value
     
@@ -48,15 +75,17 @@ class Token:
 ################################
 #LEXER
     
-class lexer:
-    def __init__(self,text) -> None:
+class Lexer:
+    def __init__(self,fn,text) -> None:
+        self.fn=fn
         self.text=text
-        self.pos=-1
+        self.pos=Position(-1,0,-1,fn,text)
         self.current_char=None
         self.advance()
+
     def advance(self):
-        self.pos+=1
-        self.current_char=self.text[self.pos]  if self.pos<len(self.text) else None
+        self.pos.advance(self.current_char)
+        self.current_char=self.text[self.pos.idx]  if self.pos.idx<len(self.text) else None
     
     def make_tokens(self):
         tokens=[]
@@ -65,30 +94,30 @@ class lexer:
             if self.current_char in " \t":
                 self.advance()
             elif self.current_char in DIGITS:
-                tokens.apppend(self.make_number())
+                tokens.append(self.make_number())
             elif self.current_char=="+":
-                tokens.append(TT_PLUS)
+                tokens.append(Token(TT_PLUS))
                 self.advance()
             elif self.current_char=="-":
-                tokens.append(TT_MINUS)
+                tokens.append(Token(TT_MINUS))
                 self.advance()
             elif self.current_char=="*":
-                tokens.append(TT_MUL)
+                tokens.append(Token(TT_MUL))
                 self.advance()
             elif self.current_char=="/":
-                tokens.append(TT_DIV)
+                tokens.append(Token(TT_DIV))
                 self.advance()
             elif self.current_char=="(":
-                tokens.append(TT_LPAREN)
+                tokens.append(Token(TT_LPAREN))
                 self.advance()
             elif self.current_char==")":
-                tokens.append(TT_RPAREN)
+                tokens.append(Token(TT_RPAREN))
                 self.advance()
             else:
+                pos_start=self.pos.copy()
                 char=self.current_char
                 self.advance()
-                return [], IllegalCharError("'"+char+"'")
-        
+                return [], IllegalCharError(pos_start,self.pos,"'"+char+"'")
         return tokens,None
     def make_number(self):
         num_str=''
@@ -101,17 +130,88 @@ class lexer:
                 num_str+="."
             else:
                 num_str+=self.current_char
+            self.advance()
                 
         if dot_count==0:
             return Token(TT_INT,int(num_str))
         else:
             return Token(TT_FLOAT,float(num_str))
+###############################
+#NODES
+
+
+class NumberNode:
+        def __init__(self,tok) -> None:
+            self.tok=tok
+
+        def __repr__(self) -> str:
+            return f'{self.tok}'
+    
+
+class BinOpNode:
+    def __init__(self,left_node,op_tok,right_node) -> None:
+        self.left_node=left_node
+        self.op_tok=op_tok
+        self.right_node=right_node
+    
+    def __repr__(self) -> str:
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+############################
+#PARSER
+
+class Parser:
+    def __init__(self,tokens) -> None:
+        self.tokens=tokens
+        self.tok_idx=-1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx+=1
+        if self.tok_idx<len(self.tokens):
+            self.current_tok=self.tokens[self.tok_idx]
+        return self.current_tok
+
+    def parse(self):
+        res=self.expr()
+        return res
+
+    def factor(self):
+        tok=self.current_tok
+
+        if tok.type in (TT_INT,TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+    def term(self):
+        return self.bin_op(self.factor,(TT_MUL,TT_DIV))
+
         
-        
-        
-        
+    def expr(self):
+        return self.bin_op(self.term,(TT_PLUS,TT_MINUS))
+
+
+    def bin_op(self,func,ops):
+        left=func()
+        while self.current_tok.type in ops:
+            op_tok=self.current_tok
+            self.advance()
+            right=func()
+            left = BinOpNode(left,op_tok,right)
+            
+        return left
+
+
+
 ############################
 #RUN
 
-def run(text):
-    
+def run(fn,text):
+    lexer=Lexer(fn,text)
+    tokens,error=lexer.make_tokens()
+    if error:return None,error
+
+    print(tokens)
+    parser=Parser(tokens)
+    ast=parser.parse()
+
+    return ast,None
