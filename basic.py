3970d3a4ea5ec7e82043705f09c58b423a851522
asqool <91128487+asqool@users.noreply.github.com>
2022-09-20 22:17:01 +0200

    1

--- basic.py
+++ basic.py
@@ -1,57 +1,9 @@
-####################################
-#CONSTANTS
+
+from re import A
+from string import digits
+
 
 DIGITS="1234567890"
-
-
-#######################################
-#ERROR
-
-
-class Error:
-    def __init__(self,pos_start,pos_end,error_name,details):
-        self.pos_start=pos_start
-        self.pos_end=pos_end
-        self.error_name=error_name
-        self.details=details
-        
-    def as_string(self):
-        result=f'{self.error_name}:{self.details}'
-        result+=f" File {self.pos_start.fn}, line {self.pos_start.ln+1}"
-        return result
-    
-class IllegalCharError(Error):
-    def __init__(self,pos_start,pos_end,details):
-        super().__init__(pos_start,pos_end,"Illegal Character",details)
-
-#######################################
-#position
-
-class Position:
-    def __init__(self,idx,ln,col,fn,ftxt) -> None:
-        self.idx=idx
-        self.col=col
-        self.ln=ln
-        self.fn=fn
-        self.ftxt=ftxt
-
-    def advance(self,current_char):
-        self.idx+=1
-        self.col+=1
-
-        if current_char=="\n":
-            self.ln+=1
-            self.col=0
-        return self
-
-
-    def copy(self):
-        return Position(self.idx,self.ln,self.col,self.fn,self.ftxt)
-
-
-########################################
-#TOKENS
-
 
 TT_INT="INT"
 TT_FLOAT="FLOAT"
@@ -59,6 +11,7 @@ TT_PLUS="PLUS"
 TT_MINUS="MINUS"
 TT_DIV="DIV"
 TT_MUL="MUL"
+TT_MOD="MOD"
 TT_LPAREN="LPAREN"
 TT_RPAREN="RPAREN"
 
@@ -72,146 +25,49 @@ class Token:
         if self.value:return  f"{self.type}:{self.value}"
         return f"{self.type}"
 
-################################
-#LEXER
-    
 class Lexer:
     def __init__(self,fn,text) -> None:
-        self.fn=fn
         self.text=text
-        self.pos=Position(-1,0,-1,fn,text)
-        self.current_char=None
-        self.advance()
+        self.fn=fn
+        self.ptr=0
 
-    def advance(self):
-        self.pos.advance(self.current_char)
-        self.current_char=self.text[self.pos.idx]  if self.pos.idx<len(self.text) else None
-    
-    def make_tokens(self):
+    def make_token(self):
         tokens=[]
-        
-        while self.current_char!=None:
-            if self.current_char in " \t":
-                self.advance()
-            elif self.current_char in DIGITS:
-                tokens.append(self.make_number())
-            elif self.current_char=="+":
+        while self.ptr<len(self.text):
+            char=self.text[self.ptr]
+            if char=="+":
                 tokens.append(Token(TT_PLUS))
-                self.advance()
-            elif self.current_char=="-":
+            elif char=="-":
                 tokens.append(Token(TT_MINUS))
-                self.advance()
-            elif self.current_char=="*":
+            elif char=="*":
                 tokens.append(Token(TT_MUL))
-                self.advance()
-            elif self.current_char=="/":
+            elif char=="/":
                 tokens.append(Token(TT_DIV))
-                self.advance()
-            elif self.current_char=="(":
+            elif char=="%":
+                tokens.append(Token(TT_MOD))
+            elif char=="(":
                 tokens.append(Token(TT_LPAREN))
-                self.advance()
-            elif self.current_char==")":
+            elif char==")":
                 tokens.append(Token(TT_RPAREN))
-                self.advance()
-            else:
-                pos_start=self.pos.copy()
-                char=self.current_char
-                self.advance()
-                return [], IllegalCharError(pos_start,self.pos,"'"+char+"'")
-        return tokens,None
+            elif char in digits:
+                self.make_number()
+
+            self.ptr+=1
+        return tokens
+
     def make_number(self):
-        num_str=''
-        dot_count=0
-        
-        while self.current_char!=None and self.current_char in DIGITS+".": 
-            if self.current_char==".":
-                if self.current_char==1:break
-                dot_count+=1
-                num_str+="."
-            else:
-                num_str+=self.current_char
-            self.advance()
-                
-        if dot_count==0:
-            return Token(TT_INT,int(num_str))
-        else:
-            return Token(TT_FLOAT,float(num_str))
-###############################
-#NODES
-
-
-class NumberNode:
-        def __init__(self,tok) -> None:
-            self.tok=tok
-
-        def __repr__(self) -> str:
-            return f'{self.tok}'
-    
-
-class BinOpNode:
-    def __init__(self,left_node,op_tok,right_node) -> None:
-        self.left_node=left_node
-        self.op_tok=op_tok
-        self.right_node=right_node
-    
-    def __repr__(self) -> str:
-        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
-
-############################
-#PARSER
-
-class Parser:
-    def __init__(self,tokens) -> None:
-        self.tokens=tokens
-        self.tok_idx=-1
-        self.advance()
-
-    def advance(self):
-        self.tok_idx+=1
-        if self.tok_idx<len(self.tokens):
-            self.current_tok=self.tokens[self.tok_idx]
-        return self.current_tok
-
-    def parse(self):
-        res=self.expr()
-        return res
-
-    def factor(self):
-        tok=self.current_tok
-
-        if tok.type in (TT_INT,TT_FLOAT):
-            self.advance()
-            return NumberNode(tok)
-    def term(self):
-        return self.bin_op(self.factor,(TT_MUL,TT_DIV))
-
-        
-    def expr(self):
-        return self.bin_op(self.term,(TT_PLUS,TT_MINUS))
-
-
-    def bin_op(self,func,ops):
-        left=func()
-        while self.current_tok.type in ops:
-            op_tok=self.current_tok
-            self.advance()
-            right=func()
-            left = BinOpNode(left,op_tok,right)
-            
-        return left
-
-
+        ptr_ini=self.ptr
 
 ############################
 #RUN
 
+
+
 def run(fn,text):
     lexer=Lexer(fn,text)
-    tokens,error=lexer.make_tokens()
-    if error:return None,error
+    token,error=lexer.make_token()
 
-    print(tokens)
-    parser=Parser(tokens)
-    ast=parser.parse()
-
-    return ast,None
+a=3
+b=a
+a+=2
+print(b,a)
