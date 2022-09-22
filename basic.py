90c462175f4ac0d03c9dc15e9a642b0453b3ce0f
asqool <axel.lleres@gmail.com>
2022-09-22 10:41:10 +0200

    1

--- basic.py
+++ basic.py
@@ -10,24 +10,38 @@ VARS={
     "CONSTANT":{},
     "PRIVATE":{}
 }
-TYPES={
-    "string":"STRING",
-    "int":"INT",
-    "float":"FLOAT",
-    "list":"LIST",
-    "dict":"DICT",
-    "bool":"BOOL",
-    "char":"CHAR"
-}
+TYPES=[
+    "string",
+    "int",
+    "float",
+    "list",
+    "dict",
+    "bool",
+    "char"
+]
 
 KEYWORDS={
     "func":"FUNCTION",
+    "function":"FUNCTION",
+    "fun":"FUNCTION",
+    "def":"FUNCTION",
     "global":"GLOBAL",
     "private":"PRIVATE",
+    "swap":"SWAP",
+    "return":"RETURN",
+    "goto":"GOTO",
+    "if":"IF"
 }
 
 LETTERS="azertyuiopqsdfghjklmmwxcvbn"
 LETTERS+=LETTERS.upper()
+
+
+
+class var:
+    def __init__(self,type_,val):
+        self.type=type_
+        self.val=val
 #######################################
 #ERROR
 
@@ -53,12 +67,23 @@ class IllegalCharError(Error):
 
 TOKENS={
     "identifier":"IDENTIFIER",
+    ".":"DOT",
+    ":":"COLON",
+    ",":"COMMA",
+    "?":"TERNAYOP",
+    "-=":"MINVARSET",
+    "+=":"PLUSVARSET",
+    "!=":"DIFF",
+    "<=":"LEESEQ",
+    ">=":"GREATEQ",
+    "<":"LESS",
+    ">":"GREAT",
     "&":"AND",
     "|":"OR",
     "=":"VARSET",
     "==":"EQ",
     "^":"POW",
-    ";":"semicol",
+    ";":"SEMICOL",
     "+":"PLUS",
     "-":"MINUS",
     "*":"MUL",
@@ -72,8 +97,6 @@ TOKENS={
     "{":"LACCO",
     "}":"RACCO"
 }
-
-
 
 class Token:
     def __init__(self,type_,value=None) -> None:
@@ -111,17 +134,32 @@ class Lexer:
             elif self.text[self.ptr]=="=" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
                     tokens.append(Token(TOKENS["=="]))
                     self.ptr+=2
+            elif self.text[self.ptr]=="-" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
+                    tokens.append(Token(TOKENS["-="]))
+                    self.ptr+=2
+            elif self.text[self.ptr]=="+" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
+                    tokens.append(Token(TOKENS["+="]))
+                    self.ptr+=2
+            elif self.text[self.ptr]=="<" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
+                    tokens.append(Token(TOKENS[">="]))
+                    self.ptr+=2
+            elif self.text[self.ptr]=="=" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
+                    tokens.append(Token(TOKENS[">="]))
+                    self.ptr+=2
+            elif self.text[self.ptr]=="!" and self.ptr+1<len(self.text) and self.text[self.ptr+1]=="=":
+                    tokens.append(Token(TOKENS["!="]))
+                    self.ptr+=2
             elif self.text[self.ptr] in LETTERS:
                 a=""
                 c=0
-                while self.text[self.ptr+c] in LETTERS:
+                while self.ptr+c <len(self.text) and self.text[self.ptr+c] in LETTERS:
                     if self.ptr+c<len(self.text):
                         a+=self.text[self.ptr+c]
                         c+=1
                 if a in KEYWORDS.keys():
                     tokens.append(Token(KEYWORDS[a]))
-                elif a in TYPES.keys():
-                    tokens.append(Token(TYPES[a]))
+                elif a in TYPES:
+                    tokens.append(Token(a))
                 else:
                     tokens.append(Token(TOKENS["identifier"],a))
                 self.ptr+=c
@@ -165,9 +203,73 @@ class Lexer:
                 return Error(None,None,"String error: ' was never closed ",self.fn)
             return Token("STRING",s)
             
-            
-            
-            
+class Function:
+    def __init__(self,tokens,name,arg=()):
+        self.tokens=tokens
+        self.name=name
+        self.arg=()
+
+class Parser:
+    def __init__(self,tokens):
+        self.tokens=tokens
+        self.ptr=0
+        
+    def getExpr(self):
+        tok_expr=[]
+        while self.ptr<len(self.tokens):
+            if self.tokens[self.ptr].type==TOKENS[";"]:
+                break
+            tok_expr.append(self.tokens[self.ptr])
+            self.ptr+=1
+        self.ptr+=1
+        
+        
+        
+    def parse(self):
+        while self.ptr<len(self.tokens):
+            if self.tokens[self.ptr].type==KEYWORDS["def"] and self.ptr+1<len(self.tokens):
+                if self.tokens[self.ptr+1].type==TOKENS["identifier"]:
+                    name=self.tokens[self.ptr+1]
+                    if self.ptr+2<len(self.tokens):
+                        lpar_idx=self.ptr+2
+                        c=3
+                        count=0
+                        rpar_dix=None
+                        while self.ptr+c<len(self.tokens):
+                            if self.tokens[self.ptr+c].type==TOKENS["("]:
+                                count+=1
+                            elif self.tokens[self.ptr+c].type==TOKENS[")"] and count>0:
+                                count-=1
+                            elif self.tokens[self.ptr+c].type==TOKENS[")"] and count==0:
+                                rpar_idx=self.ptr+c
+                                break
+                            c+=1
+                        if rpar_dix==None:
+                            pass#error '(' was never closed
+                        arg=[]
+                        a=1+lpar_idx
+                        while a<rpar_idx:
+                            if  self.tokens[a].type in TYPES and a+1<rpar_idx and self.tokens[a+1].type==TOKENS["identifier"]:
+                                arg.append({"type":self.tokens[a].type,"identifier":self.tokens[a+1].value})
+                                a+=2
+                            elif self.tokens[a].type==TOKENS[","]:
+                                a+=1
+                            else:
+                                a+=1
+                                pass #error
+                            
+                            #ensuite faut faire pareil mais pour les " { " 
+                        print(arg)            
+            elif self.tokens[self.ptr].type==KEYWORDS["private"] and self.ptr+4<len(self.tokens):
+                if self.tokens[self.ptr+1].type in TYPES and self.tokens[self.ptr+2].type==TOKENS["identifier"] and self.tokens[self.ptr+3].type==TOKENS["="]:
+                    name=self.tokens[self.ptr+2].value
+                    ty=self.tokens[self.ptr+1].type
+                    self.ptr+=4
+                    val=self.getExpr()
+                            
+                            
+            self.ptr+=1
+                
             
             
             
@@ -181,7 +283,10 @@ def run(fn,text):
     lexer=Lexer(fn,text)
     tokens=lexer.make_tokens()
     print(tokens)
+    print(VARS)
+    parser=Parser(tokens)
+    parser.parse()
 
 
-
-run("stdio","int x=1")
+run("stdio","""private int a=3+4*(5+a(8)%3);
+    """)
