14672a9318f0ea2d62835282d64b9dc985dbb456
asqool <axel.lleres@gmail.com>
2022-10-13 09:36:45 +0200

    enlever des truc

--- lexer.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    char *text;
-    text=malloc(300);
-    text="aakjhbnpm\0";
-
-    return 0;
-}
--- shell.py
+++
@@ -1,7 +0,0 @@
-import basic
-
-while True:
-    text=input("$ ->")
-    result , error=basic.run("<stdin>",text)
-    if error:print(error.as_string())
-    else: print(result)
